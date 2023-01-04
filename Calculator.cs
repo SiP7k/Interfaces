f50d0fa24adf6e602d41dbf68f4a6b396c5de126
Savva <94635067+SiP7k@users.noreply.github.com>
2023-01-04 20:01:53 -0300

    Add files via upload

---
+++ Calculator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+
+namespace HW2
+{
+    class Calculator : ISum
+    {
+        ILogger Logger { get; }
+
+        public Calculator(ILogger logger)
+        {
+            Logger = logger;
+        }
+
+        public void Sum(int firstNumber, int secondNumber)
+        {
+            Console.WriteLine($"Ответ: {firstNumber + secondNumber}");
+        }
+        public void Calculate()
+        {
+            int count = 1;
+
+            try
+            {
+                int firstNumber = Int32.Parse(TakeUserInput(ref count));
+                int secondNumber = Int32.Parse(TakeUserInput(ref count));
+                Logger.Event("Калькулятор проводит вычисления...");
+                Thread.Sleep(3000);
+                Logger.Event("Калькулятор закончил вычисления...");
+                Sum(firstNumber, secondNumber);
+            }
+            catch
+            {
+                Logger.Error("Введено некорректное значение для слагаемого");
+            }
+        }
+        private string TakeUserInput(ref int count)
+        {
+            Console.WriteLine($"Введите {count}-ое слагаемое: ");
+            count++;
+            string userInput = Console.ReadLine();
+            return userInput;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace HW2
+{
+    class Program
+    {
+        static ILogger Logger { get; set; }
+        static void Main(string[] agrs)
+        {
+            Logger = new Logger();
+            var calculator = new Calculator(Logger);
+
+            calculator.Calculate();
+            
+            
+        }
+        
+        
+    }
+    public interface ISum
+    {
+        public void Sum(){}
+    }
+    public interface ILogger
+    {
+        void Event(string message);
+        void Error(string message);
+    }
+    public class Logger : ILogger
+    {
+        public void Error(string message)
+        {
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+        public void Event(string message)
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+    }
+}
